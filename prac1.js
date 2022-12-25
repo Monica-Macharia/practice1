0a67571ab78065036fa916d0ed57f14aa1b4bd1b
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-12-25 11:57:05 +0300

    first class & high order function practice 2

--- prac1.js
+++ prac1.js
@@ -16,3 +16,52 @@
  }
 
  objholder(funvar);
+
+ //
+
+ books= [
+     {
+     name: "Forever",
+     store: 1,
+     remaining: 23
+    },{
+     name: "Love",
+     store: 2,
+     remaining: 2
+     },{
+        name: "Lover",
+        store: 3,
+        remaining: 1
+        },
+    {
+            name: "Love",
+            store: 4,
+            remaining: 3
+            }
+        ]
+//  console.log(books.book2.store)
+
+//pseudocode
+//when books are below 4 alert "restock!"
+//function loops through the object and grabs remaining[value]
+//then checks the amount against a condition
+//function that returns an alert.
+
+const loopthrough = function(arrays){
+    for(let item of arrays){  
+        if(item.remaining < 4){
+            console.log(`Restock quick: ${item.name} is at ${item.remaining}`)
+        }
+        else{
+            console.log(`Safe: ${item.name} is at ${item.remaining}`)
+        }
+        // console.log(item.remaining)
+                   
+    }
+    
+}
+
+function feeder(fun){
+    fun(books)
+}
+feeder(loopthrough);
