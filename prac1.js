d66c7fc89aec0b2ed910adb22afd757f9042dac6
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-12-25 10:29:23 +0300

    First class function

--- prac1.js
+++ prac1.js
@@ -1,5 +1,18 @@
-pseudocode
-function1 loop
-function2 hold an array and use function1 as a parameter
+//pseudocode
+//function1 loop
+//function2 hold an array and use function1 as a parameter
 
 
+//First class function
+ const funvar = function(obj){
+     for(let key in obj){
+         console.log(key)
+     }
+ }
+
+ function objholder(flows){
+        cat={name: "rosee",gender: "female", height: 78}
+        flows(cat);
+ }
+
+ objholder(funvar);
